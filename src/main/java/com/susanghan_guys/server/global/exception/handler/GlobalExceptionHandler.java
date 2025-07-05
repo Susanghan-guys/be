5b5b4979c422c4f69de3f471abdd4e5e379bcb75
zyovn <yui2507@naver.com>
2025-07-05 17:57:16 +0900

    #19 fix: 에러 로그 추가

--- src/main/java/com/susanghan_guys/server/global/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/susanghan_guys/server/global/exception/handler/GlobalExceptionHandler.java
@@ -3,18 +3,22 @@ package com.susanghan_guys.server.global.exception.handler;
 import com.susanghan_guys.server.global.common.CommonResponse;
 import com.susanghan_guys.server.global.common.code.ErrorCode;
 import com.susanghan_guys.server.global.exception.BusinessException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(BusinessException.class)
     protected ResponseEntity<CommonResponse<Void>> handleBusinessException(BusinessException e) {
         ErrorCode errorCode =  e.getErrorCode();
+        log.info("⚠️ Business Exception occurred: {}", e.getMessage());
+
         return ResponseEntity
                 .status(errorCode.getHttpStatus())
                 .body(CommonResponse.failure(errorCode));
@@ -30,6 +34,8 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     protected ResponseEntity<CommonResponse<Void>> handleUnexpectedException(Exception e) {
+        log.error("🚨 Unexpected Error Log: {}",e.getMessage());
+
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(CommonResponse.failure(ErrorCode.INTERNAL_SERVER_ERROR));
