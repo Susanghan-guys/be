b7d289b08852e4d02a6e8f8917345e49e84c2c3c
zyovn <yui2507@naver.com>
2025-07-08 20:57:27 +0900

    #26 docs: swagger 작성

--- src/main/java/com/susanghan_guys/server/user/presentation/UserAuthController.java
+++ src/main/java/com/susanghan_guys/server/user/presentation/UserAuthController.java
@@ -5,7 +5,7 @@ import com.susanghan_guys.server.global.jwt.JwtProvider;
 import com.susanghan_guys.server.user.application.UserAuthService;
 import com.susanghan_guys.server.user.dto.request.RefreshTokenRequest;
 import com.susanghan_guys.server.user.dto.response.RefreshTokenResponse;
-import io.swagger.v3.oas.annotations.tags.Tag;
+import com.susanghan_guys.server.user.presentation.swagger.UserAuthSwagger;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -17,23 +17,24 @@ import org.springframework.web.bind.annotation.RestController;
 import static com.susanghan_guys.server.global.common.code.SuccessCode.USER_LOGOUT_SUCCESS;
 import static com.susanghan_guys.server.global.common.code.SuccessCode.REFRESH_TOKEN_SUCCESS;
 
-@Tag(name = "[사용자 회원가입/로그인]", description = "사용자 회원가입, 로그인 관련 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1/auth")
-public class UserAuthController {
+public class UserAuthController implements UserAuthSwagger {
 
     private final JwtProvider jwtProvider;
     private final UserAuthService userAuthService;
 
+    @Override
     @PostMapping("/logout")
     public CommonResponse<String> logout(HttpServletRequest request) {
         userAuthService.logout(jwtProvider.resolveToken(request));
         return CommonResponse.success(USER_LOGOUT_SUCCESS, "OK");
     }
 
+    @Override
     @PostMapping("/refresh-token")
-    public CommonResponse<RefreshTokenResponse> refreshToken(@RequestBody @Valid RefreshTokenRequest request) {
+    public CommonResponse<RefreshTokenResponse> updateRefreshToken(@RequestBody @Valid RefreshTokenRequest request) {
         RefreshTokenResponse refreshTokenResponse = userAuthService.refreshTokenResponse(request.refreshToken());
         return CommonResponse.success(REFRESH_TOKEN_SUCCESS, refreshTokenResponse);
     }
---
+++ src/main/java/com/susanghan_guys/server/user/presentation/swagger/UserAuthSwagger.java
@@ -0,0 +1,41 @@
+package com.susanghan_guys.server.user.presentation.swagger;
+
+import com.susanghan_guys.server.global.common.CommonResponse;
+import com.susanghan_guys.server.user.dto.request.RefreshTokenRequest;
+import com.susanghan_guys.server.user.dto.response.RefreshTokenResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Tag(name = "[사용자 회원가입/로그인]", description = "사용자 회원가입, 로그인 관련 API")
+public interface UserAuthSwagger {
+    @Operation(
+            summary = "사용자 로그아웃 API",
+            description = "사용자 로그아웃을 진행합니다."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "사용자 로그아웃이 성공적으로 실행되었습니다."
+            )
+    })
+    CommonResponse<String> logout(HttpServletRequest request);
+
+    @Operation(
+            summary = "refreshToken 재발급 API",
+            description = "refreshToken 재발급을 진행합니다."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "refreshToken 재발급이 성공적으로 실행되었습니다."
+            )
+    })
+    CommonResponse<RefreshTokenResponse> updateRefreshToken(
+            @RequestBody @Valid RefreshTokenRequest request
+    );
+}
