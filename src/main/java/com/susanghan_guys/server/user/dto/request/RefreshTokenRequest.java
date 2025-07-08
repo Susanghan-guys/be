469adf82605e185c533e73059dce75074871578b
zyovn <yui2507@naver.com>
2025-07-08 22:00:48 +0900

    #26 docs: schema 작성

--- src/main/java/com/susanghan_guys/server/user/dto/request/RefreshTokenRequest.java
+++ src/main/java/com/susanghan_guys/server/user/dto/request/RefreshTokenRequest.java
@@ -1,6 +1,15 @@
 package com.susanghan_guys.server.user.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+
+@Builder
+@Schema(description = "refreshToken 재발급")
 public record RefreshTokenRequest(
+        @Schema(
+                description = "만료된 refreshToken",
+                example = "eyJh.eqi57hK"
+        )
         String refreshToken
 ) {
 }
