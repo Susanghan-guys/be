66a89e6973b4ec2e02ad5ae36cb2523aed8125bd
zyovn <yui2507@naver.com>
2025-07-08 18:12:43 +0900

    #23 fix: 피드백 반영

--- src/main/java/com/susanghan_guys/server/global/oauth2/infrastructure/userinfo/KakaoUserInfo.java
+++ src/main/java/com/susanghan_guys/server/global/oauth2/infrastructure/userinfo/KakaoUserInfo.java
@@ -4,7 +4,6 @@ import com.susanghan_guys.server.global.oauth2.domain.OAuth2UserInfo;
 import com.susanghan_guys.server.user.domain.type.SocialLogin;
 import lombok.RequiredArgsConstructor;
 
-import java.util.LinkedHashMap;
 import java.util.Map;
 
 @RequiredArgsConstructor
@@ -20,13 +19,22 @@ public class KakaoUserInfo implements OAuth2UserInfo {
     @Override
     public String getEmail() {
         Map<String, Object> accountMap = (Map<String, Object>) attributes.get("kakao_account");
+        if (accountMap == null) {
+            return null;
+        }
         return (String) accountMap.get("email");
     }
 
     @Override
     public String getName() {
         Map<String, Object> accountMap = (Map<String, Object>) attributes.get("kakao_account");
+        if (accountMap == null) {
+            return null;
+        }
         Map<String, Object> profileMap = (Map<String, Object>) accountMap.get("profile");
+        if (profileMap == null) {
+            return null;
+        }
         return (String) profileMap.get("nickname");
     }
 
