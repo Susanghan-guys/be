c5e484f4a45200f27dca2a9cd661e1e87289ecb3
zyovn <yui2507@naver.com>
2025-07-09 14:01:36 +0900

    #28 remove: terms 관련 도메인 삭제

--- src/main/java/com/susanghan_guys/server/terms/infrastructure/persistence/TermsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.susanghan_guys.server.terms.infrastructure.persistence;
-
-import com.susanghan_guys.server.terms.domain.Terms;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TermsRepository extends JpaRepository<Terms, Long> {
-}
--- src/main/java/com/susanghan_guys/server/terms/infrastructure/persistence/UserTermsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.susanghan_guys.server.terms.infrastructure.persistence;
-
-import com.susanghan_guys.server.terms.domain.UserTerms;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserTermsRepository extends JpaRepository<UserTerms, Long> {
-}
