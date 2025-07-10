46eb54aff2e14c49b482da625375bea495283a17
zyovn <yui2507@naver.com>
2025-07-10 15:19:30 +0900

    #36 fix: frontend redirect uri 추가

--- src/main/java/com/susanghan_guys/server/global/oauth2/handler/OAuth2SuccessHandler.java
+++ src/main/java/com/susanghan_guys/server/global/oauth2/handler/OAuth2SuccessHandler.java
@@ -1,6 +1,5 @@
 package com.susanghan_guys.server.global.oauth2.handler;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.susanghan_guys.server.global.jwt.JwtProvider;
 import com.susanghan_guys.server.global.oauth2.domain.RefreshToken;
 import com.susanghan_guys.server.global.oauth2.infrastructure.persistence.RefreshTokenRepository;
@@ -9,12 +8,14 @@ import com.susanghan_guys.server.user.domain.User;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
-import java.util.Map;
 
 @Component
 @RequiredArgsConstructor
@@ -22,7 +23,9 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
 
     private final JwtProvider jwtProvider;
     private final RefreshTokenRepository refreshTokenRepository;
-    private final ObjectMapper objectMapper;
+
+    @Value("${frontend.oauth2.redirect-uri}")
+    private String redirectUri;
 
     @Override
     public void onAuthenticationSuccess(
@@ -33,16 +36,24 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
         CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
         User user = customUserDetails.getUser();
 
+        String provider = ((OAuth2AuthenticationToken) authentication).getAuthorizedClientRegistrationId();
+
         String accessToken = jwtProvider.createAccessToken(user.getId());
         String refreshToken = jwtProvider.createRefreshToken(user.getId());
 
         refreshTokenRepository.save(new RefreshToken(user.getId(), refreshToken));
 
-        Map<String, Object> tokens = Map.of(
-                "accessToken", accessToken,
-                "refreshToken", refreshToken
-        );
-        response.setContentType("application/json;charset=UTF-8");
-        objectMapper.writeValue(response.getWriter(), tokens);
+        String redirectUri = createRedirectUri(provider, accessToken, refreshToken);
+        response.sendRedirect(redirectUri);
+    }
+
+    private String createRedirectUri(String provider, String accessToken, String refreshToken) {
+        return UriComponentsBuilder
+                .fromUriString(redirectUri)
+                .queryParam("provider", provider)
+                .queryParam("accessToken", accessToken)
+                .queryParam("refreshToken", refreshToken)
+                .build()
+                .toString();
     }
 }
