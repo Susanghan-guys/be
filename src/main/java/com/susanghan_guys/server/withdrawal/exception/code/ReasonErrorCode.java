7d7a76dc1bcddb3a18f6ca33f575a4735bb67370
zyovn <yui2507@naver.com>
2025-07-20 03:22:59 +0900

    #56 fix: userException -> reasonException으로 수정

--- src/main/java/com/susanghan_guys/server/withdrawal/exception/code/ReasonErrorCode.java
+++ src/main/java/com/susanghan_guys/server/withdrawal/exception/code/ReasonErrorCode.java
@@ -1,4 +1,4 @@
-package com.susanghan_guys.server.user.exception.code;
+package com.susanghan_guys.server.withdrawal.exception.code;
 
 import com.susanghan_guys.server.global.common.code.BaseCode;
 import lombok.Getter;
@@ -7,12 +7,10 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 @RequiredArgsConstructor
-public enum UserErrorCode implements BaseCode {
+public enum ReasonErrorCode implements BaseCode {
 
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, 404, "존재하지 않는 사용자입니다."),
-    REQUIRED_TERMS_NOT_AGREED(HttpStatus.BAD_REQUEST, 400, "필수 약관에 동의하지 않았습니다."),
     ETC_DETAIL_REQUIRED(HttpStatus.BAD_REQUEST, 400, "기타일 경우, 상세 내용을 작성해야 합니다."),
-    USER_ALREADY_DELETED(HttpStatus.CONFLICT, 409, "이미 탈퇴한 사용자 입니다."),
+    WITHDRAWAL_REASON_REQUIRED(HttpStatus.BAD_REQUEST, 400, "탈퇴 이유가 선택되지 않았습니다."),
     ;
 
     private final HttpStatus httpStatus;
--- src/main/java/com/susanghan_guys/server/withdrawal/validator/ReasonValidator.java
+++ src/main/java/com/susanghan_guys/server/withdrawal/validator/ReasonValidator.java
@@ -1,9 +1,9 @@
 package com.susanghan_guys.server.withdrawal.validator;
 
 import com.susanghan_guys.server.user.dto.request.UserWithdrawalRequest;
-import com.susanghan_guys.server.user.exception.UserException;
-import com.susanghan_guys.server.user.exception.code.UserErrorCode;
 import com.susanghan_guys.server.withdrawal.domain.type.ReasonType;
+import com.susanghan_guys.server.withdrawal.exception.ReasonException;
+import com.susanghan_guys.server.withdrawal.exception.code.ReasonErrorCode;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -14,12 +14,12 @@ public class ReasonValidator {
     public void validateWithdrawalReason(UserWithdrawalRequest request) {
         List<ReasonType> reasons = request.withdrawalReasons();
         if (reasons == null || reasons.isEmpty()) {
-            throw new UserException(UserErrorCode.WITHDRAWAL_REASON_REQUIRED);
+            throw new ReasonException(ReasonErrorCode.WITHDRAWAL_REASON_REQUIRED);
         }
 
         if (reasons.contains(ReasonType.ETC)) {
             if (request.etc() == null || request.etc().isBlank()) {
-                throw new UserException(UserErrorCode.ETC_DETAIL_REQUIRED);
+                throw new ReasonException(ReasonErrorCode.ETC_DETAIL_REQUIRED);
             }
         }
     }
