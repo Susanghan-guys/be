937b576a779de2a53284637bbed1562653ca95b4
zyovn <yui2507@naver.com>
2025-07-25 14:04:26 +0900

    #68 fix: category json property 수정

--- src/main/java/com/susanghan_guys/server/work/domain/type/Category.java
+++ src/main/java/com/susanghan_guys/server/work/domain/type/Category.java
@@ -1,5 +1,20 @@
 package com.susanghan_guys.server.work.domain.type;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public enum Category {
-    VISUAL, FILM, DIGICON, EXP, OUTACT
+    @JsonProperty("Visual")
+    VISUAL,
+
+    @JsonProperty("Film")
+    FILM,
+
+    @JsonProperty("Digital Contents")
+    DIGICON,
+
+    @JsonProperty("Experience")
+    EXP,
+
+    @JsonProperty("Outdoor Activation")
+    OUTACT
 }
