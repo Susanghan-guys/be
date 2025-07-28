76d9f15d3b2b03e0a693a2d606f984eeb6e0c4c2
zyovn <yui2507@naver.com>
2025-07-29 04:23:37 +0900

    #67 refactor: successCode 도메인 별로 분리

--- src/main/java/com/susanghan_guys/server/global/common/code/SuccessCode.java
+++ src/main/java/com/susanghan_guys/server/global/common/code/SuccessCode.java
@@ -11,18 +11,6 @@ public enum SuccessCode implements BaseCode {
     // health_check
     HEALTH_CHECK_SUCCESS(HttpStatus.OK, 200, "Health Check Success"),
 
-    // token
-    REFRESH_TOKEN_UPDATE_SUCCESS(HttpStatus.OK, 200, "Refresh Token Update Success"),
-    EXCHANGE_TOKEN_SUCCESS(HttpStatus.OK, 200, "Exchange Token Success"),
-
-    // user
-    USER_LOGOUT_SUCCESS(HttpStatus.OK, 200, "User Logout Success"),
-    USER_AGREEMENT_SUCCESS(HttpStatus.OK, 200, "User Agreement Success"),
-    USER_ONBOARDING_SUCCESS(HttpStatus.OK, 200, "User Onboarding Success"),
-    USER_INFO_SUCCESS(HttpStatus.OK, 200, "User Info Success"),
-    USER_INFO_UPDATE_SUCCESS(HttpStatus.OK, 200, "User Info Update Success"),
-    USER_WITHDRAWAL_SUCCESS(HttpStatus.OK, 200, "User Withdrawal Success"),
-
     // work
     WORK_DCA_SUBMIT_SUCCESS(HttpStatus.OK, 200, "DCA Work Submission Success"),
     WORK_YCC_SUBMIT_SUCCESS(HttpStatus.OK, 200, "YCC Work Submission Success"),
