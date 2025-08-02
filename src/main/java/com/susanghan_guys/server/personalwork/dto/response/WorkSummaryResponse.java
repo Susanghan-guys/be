62ff03343b13dfd1e18e3848355d69c291129e99
zyovn <yui2507@naver.com>
2025-08-03 00:24:18 +0900

    #67 docs: swagger 작성

--- src/main/java/com/susanghan_guys/server/personalwork/dto/response/WorkSummaryResponse.java
+++ src/main/java/com/susanghan_guys/server/personalwork/dto/response/WorkSummaryResponse.java
@@ -1,11 +1,27 @@
 package com.susanghan_guys.server.personalwork.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
+@Schema(description = "출품작 요약 생성")
 public record WorkSummaryResponse(
+        @Schema(
+                description = "Target",
+                example = "정기 후원 참여율이 낮은 20~39세 MZ세대. 공감보다 자기 연결감을 중시하며, 개인의 가치에 기반한 메시지에 반응한다."
+        )
         String target,
+
+        @Schema(
+                description = "Insight",
+                example = "2030세대는 자신과 닮았다고 느끼는 대상에게 감정적으로 더 쉽게 연결되며, ‘투영’을 통해 연대감을 느낀다."
+        )
         String insight,
+
+        @Schema(
+                description = "Solution",
+                example = "수혜자에게 후원자의 어린 시절을 투영하게 하여, 정서적 연결을 통해 기부 행동으로 자연스럽게 이어지도록 유도한다."
+        )
         String solution
 ) {
 }
--- src/main/java/com/susanghan_guys/server/personalwork/presentation/PersonalWorkController.java
+++ src/main/java/com/susanghan_guys/server/personalwork/presentation/PersonalWorkController.java
@@ -3,6 +3,7 @@ package com.susanghan_guys.server.personalwork.presentation;
 import com.susanghan_guys.server.global.common.CommonResponse;
 import com.susanghan_guys.server.personalwork.application.PersonalWorkService;
 import com.susanghan_guys.server.personalwork.dto.response.WorkSummaryResponse;
+import com.susanghan_guys.server.personalwork.presentation.swagger.PersonalWorkSwagger;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,15 +12,17 @@ import static com.susanghan_guys.server.personalwork.presentation.response.Perso
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1/personal-works")
-public class PersonalWorkController {
+public class PersonalWorkController implements PersonalWorkSwagger {
 
     private final PersonalWorkService personalWorkService;
 
+    @Override
     @PostMapping("/dca/{workId}/summary")
     public CommonResponse<WorkSummaryResponse> createDcaWorkSummary(@PathVariable(name = "workId") Long workId) {
         return CommonResponse.success(WORK_SUMMARY_SUCCESS, personalWorkService.createDcaWorkSummary(workId));
     }
 
+    @Override
     @PostMapping("/ycc/{workId}/summary")
     public CommonResponse<WorkSummaryResponse> createYccWorkSummary(@PathVariable(name = "workId") Long workId) {
         return CommonResponse.success(WORK_SUMMARY_SUCCESS, personalWorkService.createYccWorkSummary(workId));
