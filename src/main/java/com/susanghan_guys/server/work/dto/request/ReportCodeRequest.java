69146b4ea60a96f43361ea7ecd48aa2c1bcbfe19
zyovn <yui2507@naver.com>
2025-08-10 01:46:25 +0900

    #88 docs: schema 작성

--- src/main/java/com/susanghan_guys/server/work/dto/request/ReportCodeRequest.java
+++ src/main/java/com/susanghan_guys/server/work/dto/request/ReportCodeRequest.java
@@ -1,9 +1,12 @@
 package com.susanghan_guys.server.work.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
+@Schema(description = "리포트 코드 인증 API")
 public record ReportCodeRequest(
+        @Schema(description = "발급된 리포트 코드", example = "7E9IU8")
         String code
 ) {
 }
--- src/main/java/com/susanghan_guys/server/work/dto/request/ReportDeletionRequest.java
+++ src/main/java/com/susanghan_guys/server/work/dto/request/ReportDeletionRequest.java
@@ -1,9 +1,12 @@
 package com.susanghan_guys.server.work.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
+@Schema(description = "리포트 삭제 API (신청자일 경우, 삭제 불가능)")
 public record ReportDeletionRequest(
+        @Schema(description = "출품작 제목", example = "너에게서 나를 보다")
         String title
 ) {
 }
