7071ef73f27619cc140491d65985d9a0c335bf2d
zyovn <yui2507@naver.com>
2025-08-10 00:58:05 +0900

    #88 chore: 불필요한 import문 삭제

--- src/main/java/com/susanghan_guys/server/work/exception/WorkException.java
+++ src/main/java/com/susanghan_guys/server/work/exception/WorkException.java
@@ -1,12 +1,8 @@
 package com.susanghan_guys.server.work.exception;
 
-
-import com.susanghan_guys.server.global.common.code.BaseCode;
 import com.susanghan_guys.server.global.exception.BusinessException;
-import com.susanghan_guys.server.user.exception.code.UserErrorCode;
 import com.susanghan_guys.server.work.exception.code.WorkErrorCode;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
 public class WorkException extends BusinessException {
