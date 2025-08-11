d11bc4f79f54bda168cdaa0c6711c7e312b309f4
zyovn <yui2507@naver.com>
2025-08-11 23:35:33 +0900

    #91 refactor: evaludationType 라벨 추가

--- src/main/java/com/susanghan_guys/server/personalwork/domain/type/EvaluationType.java
+++ src/main/java/com/susanghan_guys/server/personalwork/domain/type/EvaluationType.java
@@ -4,18 +4,20 @@ import lombok.Getter;
 
 @Getter
 public enum EvaluationType {
-    TARGET_FITNESS("prompt"),
-    BRAND_UNDERSTANDING("prompt"),
-    MEDIA_SELECTION("prompt"),
-    PROBLEM_DEFINITION("prompt"),
-    FEASIBILITY("prompt"),
-    AGENDA_SELECTION("prompt"),
-    INFLUENCE("prompt"),
-    DELIVERY("prompt");
+    TARGET_FITNESS("타겟 적합성", "prompt"),
+    BRAND_UNDERSTANDING("브랜드 이해도", "prompt"),
+    MEDIA_SELECTION("매체 선정", "prompt"),
+    PROBLEM_DEFINITION("문제 정의", "prompt"),
+    FEASIBILITY("실현 가능성", "prompt"),
+    AGENDA_SELECTION("아젠다 선정", "prompt"),
+    INFLUENCE("영향력", "prompt"),
+    DELIVERY("전달력", "prompt");
 
+    private final String label;
     private final String prompt;
 
-    EvaluationType(String prompt) {
+    EvaluationType(String label, String prompt) {
+        this.label = label;
         this.prompt = prompt;
     }
 }
