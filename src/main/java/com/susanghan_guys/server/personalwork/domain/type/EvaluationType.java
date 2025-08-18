985a9154d571ad295a105ccf673ecf5372861f3c
zyovn <yui2507@naver.com>
2025-08-19 00:06:16 +0900

    #90 refactor: evaludationType내 prompt 삭제

--- src/main/java/com/susanghan_guys/server/personalwork/domain/type/EvaluationType.java
+++ src/main/java/com/susanghan_guys/server/personalwork/domain/type/EvaluationType.java
@@ -5,24 +5,22 @@ import lombok.Getter;
 
 @Getter
 public enum EvaluationType {
-    TARGET_FITNESS("타겟 적합성", "prompt", WorkType.DCA_ALL),
-    BRAND_UNDERSTANDING("브랜드 이해도", "prompt", WorkType.DCA_ALL),
-    MEDIA_SELECTION("매체 선정", "prompt", WorkType.DCA_YCC),
-    PROBLEM_DEFINITION("문제 정의", "prompt", WorkType.DCA_ALL),
-    FEASIBILITY("실현 가능성", "prompt", WorkType.DCA_YCC),
-    STORYTELLING("스토리텔링", "prompt", WorkType.DCA_FILM),
-    DIRECTION("연출", "prompt", WorkType.DCA_FILM),
-    AGENDA_SELECTION("아젠다 선정", "prompt", WorkType.YCC),
-    INFLUENCE("영향력", "prompt", WorkType.YCC),
-    DELIVERY("전달력", "prompt", WorkType.YCC);
+    TARGET_FITNESS("타겟 적합성", WorkType.DCA_ALL),
+    BRAND_UNDERSTANDING("브랜드 이해도", WorkType.DCA_ALL),
+    MEDIA_SELECTION("매체 선정", WorkType.DCA_YCC),
+    PROBLEM_DEFINITION("문제 정의", WorkType.DCA_ALL),
+    FEASIBILITY("실현 가능성", WorkType.DCA_YCC),
+    STORYTELLING("스토리텔링", WorkType.DCA_FILM),
+    DIRECTION("연출", WorkType.DCA_FILM),
+    AGENDA_SELECTION("아젠다 선정", WorkType.YCC),
+    INFLUENCE("영향력", WorkType.YCC),
+    DELIVERY("전달력", WorkType.YCC);
 
     private final String label;
-    private final String prompt;
     private final WorkType type;
 
-    EvaluationType(String label, String prompt, WorkType type) {
+    EvaluationType(String label, WorkType type) {
         this.label = label;
-        this.prompt = prompt;
         this.type = type;
     }
 }
