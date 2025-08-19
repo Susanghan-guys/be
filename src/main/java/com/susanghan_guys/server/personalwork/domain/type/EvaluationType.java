0bbadf69ef96f170fae3bdc225076ed6fd268b08
zyovn <yui2507@naver.com>
2025-08-20 02:52:08 +0900

    #90 refactor: ycc 세부 총평 프롬프트 2차 수정

--- src/main/java/com/susanghan_guys/server/personalwork/domain/type/EvaluationType.java
+++ src/main/java/com/susanghan_guys/server/personalwork/domain/type/EvaluationType.java
@@ -3,17 +3,17 @@ package com.susanghan_guys.server.personalwork.domain.type;
 import com.susanghan_guys.server.work.domain.type.WorkType;
 import lombok.Getter;
 
-import static com.susanghan_guys.server.work.domain.type.WorkType.*;
-
 @Getter
 public enum EvaluationType {
     TARGET_FITNESS("타겟 적합성", WorkType.DCA_ALL),
     BRAND_UNDERSTANDING("브랜드 이해도", WorkType.DCA_ALL),
-    MEDIA_SELECTION("매체 선정", WorkType.DCA_YCC),
-    PROBLEM_DEFINITION("문제 정의", DCA_ALL),
-    FEASIBILITY("실현 가능성", WorkType.DCA_YCC),
-    STORYTELLING("스토리텔링", DCA_FILM),
-    DIRECTION("연출", DCA_FILM),
+    DCA_MEDIA_SELECTION("매체 선정", WorkType.DCA),
+    PROBLEM_DEFINITION("문제 정의", WorkType.DCA_ALL),
+    DCA_FEASIBILITY("실현 가능성", WorkType.DCA),
+    STORYTELLING("스토리텔링", WorkType.DCA_FILM),
+    DIRECTION("연출", WorkType.DCA_ALL),
+    YCC_FEASIBILITY("실현 가능성", WorkType.YCC),
+    YCC_MEDIA_SELECTION("매체 선정", WorkType.YCC),
     AGENDA_SELECTION("아젠다 선정", WorkType.YCC),
     INFLUENCE("영향력", WorkType.YCC),
     DELIVERY("전달력", WorkType.YCC);
