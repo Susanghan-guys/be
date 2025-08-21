5513d8f79f03b8430ae706aa3221771bd170b723
Kang Sujin <151008307+nnmrm@users.noreply.github.com>
2025-08-22 07:43:27 +0900

    #96 feat: 브랜드 브리프 전용 테이블 추가

---
+++ src/main/java/com/susanghan_guys/server/personalwork/domain/BrandBrief.java
@@ -0,0 +1,34 @@
+package com.susanghan_guys.server.personalwork.domain;
+
+import com.susanghan_guys.server.work.domain.type.Brand;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Getter
+@Entity
+@Table(name = "brand_brief")
+public class BrandBrief {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "brand", nullable = false)
+    private Brand brand;
+
+    @Column(name = "brand_intro", nullable = false, columnDefinition = "TEXT")
+    private String brandIntro;
+
+    @Column(name = "market_status", nullable = false, columnDefinition = "TEXT")
+    private String marketStatus;
+
+    @Column(name = "brand_comm_target", nullable = false, columnDefinition = "TEXT")
+    private String brandCommTarget;
+
+    @Column(name = "challenge", nullable = false, columnDefinition = "TEXT")
+    private String challenge;
+
+    @Column(name = "cautions", nullable = false, columnDefinition = "TEXT")
+    private String cautions;
+
+}
