6b219564ce75a4497559724429f506ad1e7beba9
zyovn <yui2507@naver.com>
2025-08-22 03:05:59 +0900

    #100 feat: workSummary db 저장 로직 구현

--- src/main/java/com/susanghan_guys/server/personalwork/infrastructure/persistence/SummaryRepository.java
+++ src/main/java/com/susanghan_guys/server/personalwork/infrastructure/persistence/SummaryRepository.java
@@ -3,5 +3,8 @@ package com.susanghan_guys.server.personalwork.infrastructure.persistence;
 import com.susanghan_guys.server.personalwork.domain.Summary;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface SummaryRepository extends JpaRepository<Summary, Long> {
+    Optional<Summary> findByWorkId(Long workId);
 }
