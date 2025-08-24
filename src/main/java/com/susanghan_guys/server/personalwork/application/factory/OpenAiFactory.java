25d5be8807e0f82810eae0232a01a4dacf96f465
zyovn <yui2507@naver.com>
2025-08-24 17:23:13 +0900

    #100 fix: pr과 겹치는 부분 제거

--- src/main/java/com/susanghan_guys/server/personalwork/application/factory/OpenAiFactory.java
+++ src/main/java/com/susanghan_guys/server/personalwork/application/factory/OpenAiFactory.java
@@ -5,11 +5,9 @@ import com.susanghan_guys.server.global.client.openai.OpenAiRequest;
 import com.susanghan_guys.server.personalwork.application.port.PdfFilePort;
 import com.susanghan_guys.server.personalwork.exception.PersonalWorkException;
 import com.susanghan_guys.server.personalwork.exception.code.PersonalWorkErrorCode;
-import com.susanghan_guys.server.work.infrastructure.persistence.WorkRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
@@ -17,29 +15,7 @@ import java.util.Objects;
 @RequiredArgsConstructor
 public class OpenAiFactory {
 
-    private final WorkRepository workRepository;
     private final PdfFilePort pdfFilePort;
-
-    public OpenAiRequest buildDcaOpenAiRequest(Long workId) {
-        // FIXME: DIP 위반 -> 추후 수정 필수.
-        List<String> imageUrls = new ArrayList<>(workRepository.findWorkContentByWorkId(workId));
-
-        List<PdfImage> pdfImages = pdfFilePort.convertDcaPdfToImage(workId);
-
-        // 추가 파일(기획안)이 존재할 경우, 브리프 보드 + 추가 파일(기획안) 함께 전송
-        imageUrls.addAll(
-                pdfImages.stream()
-                        .map(PdfImage::getImageUrl)
-                        .filter(Objects::nonNull)
-                        .toList()
-        );
-
-        if (imageUrls.isEmpty()) {
-            throw new PersonalWorkException(PersonalWorkErrorCode.WORK_IMAGE_NOT_FOUND);
-        }
-
-        return new OpenAiRequest(imageUrls);
-    }
 
     public OpenAiRequest buildYccOpenAiRequest(Long workId) {
         List<String> imageUrls = pdfFilePort.convertYccPdfToImage(workId).stream()
