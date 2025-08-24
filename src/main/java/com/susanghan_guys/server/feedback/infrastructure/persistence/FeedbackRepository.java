fe7f6f89401c2ab498b40d88aa51502f9b5db013
zyovn <yui2507@naver.com>
2025-08-24 16:32:11 +0900

    #100 feat: feedbackRepository 구현

---
+++ src/main/java/com/susanghan_guys/server/feedback/infrastructure/persistence/FeedbackRepository.java
@@ -0,0 +1,7 @@
+package com.susanghan_guys.server.feedback.infrastructure.persistence;
+
+import com.susanghan_guys.server.feedback.domain.Feedback;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
+}
