89d7b35451b39e34cef0752b9f251d6687d49d5d
Kang Sujin <151008307+nnmrm@users.noreply.github.com>
2025-08-28 09:46:32 +0900

    refactor: dca, ycc 총평 컨트롤러 병합

--- src/main/java/com/susanghan_guys/server/personalwork/presentation/WorkEvaluationController.java
+++ src/main/java/com/susanghan_guys/server/personalwork/presentation/WorkEvaluationController.java
@@ -2,10 +2,12 @@ package com.susanghan_guys.server.personalwork.presentation;
 
 import com.susanghan_guys.server.global.common.CommonResponse;
 import com.susanghan_guys.server.personalwork.application.DcaWorkEvaluationService;
+import com.susanghan_guys.server.personalwork.application.YccWorkEvaluationService;
 import com.susanghan_guys.server.personalwork.domain.type.EvaluationType;
 import com.susanghan_guys.server.personalwork.dto.response.DcaWorkEvaluationResponse;
 import com.susanghan_guys.server.personalwork.dto.response.DetailEvaluationResponse;
-import com.susanghan_guys.server.personalwork.presentation.swagger.DcaWorkEvaluationSwagger;
+import com.susanghan_guys.server.personalwork.dto.response.YccWorkEvaluationResponse;
+import com.susanghan_guys.server.personalwork.presentation.swagger.WorkEvaluationSwagger;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,13 +15,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import static com.susanghan_guys.server.personalwork.presentation.response.PersonalWorkSuccessCode.*;
+import static com.susanghan_guys.server.personalwork.presentation.response.PersonalWorkSuccessCode.DCA_WORK_DETAIL_EVALUATION_SUCCESS;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1/personal-works")
-public class DcaWorkEvaluationController implements DcaWorkEvaluationSwagger {
+public class WorkEvaluationController implements WorkEvaluationSwagger {
 
+    private final YccWorkEvaluationService yccWorkEvaluationService;
     private final DcaWorkEvaluationService dcaWorkEvaluationService;
+
+    @Override
+    @PostMapping("/ycc/{workId}/evaluation")
+    public CommonResponse<YccWorkEvaluationResponse> createYccWorkEvaluation(@PathVariable Long workId) {
+        return CommonResponse.success(YCC_WORK_EVALUATION_SUCCESS, yccWorkEvaluationService.createYccWorkEvaluation(workId));
+    }
+
+    @Override
+    @PostMapping("/ycc/{workId}/evaluation/{type}")
+    public CommonResponse<DetailEvaluationResponse> createYccDetailEvaluation(
+            @PathVariable Long workId,
+            @PathVariable EvaluationType type
+    ) {
+        return CommonResponse.success(YCC_WORK_DETAIL_EVALUATION_SUCCESS, yccWorkEvaluationService.createYccDetailEvaluation(workId, type));
+    }
 
     @Override
     @PostMapping("/dca/{workId}/evaluation")
--- src/main/java/com/susanghan_guys/server/personalwork/presentation/swagger/WorkEvaluationSwagger.java
+++ src/main/java/com/susanghan_guys/server/personalwork/presentation/swagger/WorkEvaluationSwagger.java
@@ -4,6 +4,7 @@ import com.susanghan_guys.server.global.common.CommonResponse;
 import com.susanghan_guys.server.personalwork.domain.type.EvaluationType;
 import com.susanghan_guys.server.personalwork.dto.response.DcaWorkEvaluationResponse;
 import com.susanghan_guys.server.personalwork.dto.response.DetailEvaluationResponse;
+import com.susanghan_guys.server.personalwork.dto.response.YccWorkEvaluationResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
@@ -11,7 +12,50 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Tag(name = "[출품작 총평]", description = "작품 총평 관련 API")
-public interface DcaWorkEvaluationSwagger {
+public interface WorkEvaluationSwagger {
+    @Operation(
+            summary = "YCC 출품작 전체 총평 API",
+            description = """
+                    ### Pathvariable
+                    ---
+                    `workId` : 작품 고유 ID
+                    """
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "YCC 출품작에 대한 전체 총평이 성공적으로 생성되었습니다."
+            )
+    })
+    CommonResponse<YccWorkEvaluationResponse> createYccWorkEvaluation(
+            @PathVariable Long workId
+    );
+
+    @Operation(
+            summary = "YCC 출품작 세부 총평 API",
+            description = """
+                    ### Pathvariable
+                    ---
+                    `workId` : 작품 고유 ID \n
+                    `type`: YCC 전체 총평 카테고리
+                    - `YCC_FEASIBILITY`: 실현 가능성
+                    - `YCC_MEDIA_SELECTION`: 매체 선정
+                    - `AGENDA_SELECTION`: 아젠다 선정
+                    - `INFLUENCE`: 영향력
+                    - `DELIVERY`: 전달력
+                    """
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "YCC 출품작에 대한 세부 총평이 성공적으로 생성되었습니다."
+            )
+    })
+    CommonResponse<DetailEvaluationResponse> createYccDetailEvaluation(
+            @PathVariable Long workId,
+            @PathVariable EvaluationType type
+    );
+
     @Operation(
             summary = "DCA 출품작 전체 총평 API",
             description = """
@@ -52,4 +96,5 @@ public interface DcaWorkEvaluationSwagger {
             @PathVariable Long workId,
             @PathVariable EvaluationType type
     );
+
 }
